8d4af7f50658777b69a3638479821dc43b47682d
Luis Martinez <luis_mmartinez@live.com>
2023-11-14 14:38:49 -0500

    add haml gem

--- app/views/feature/feature.js
+++ app/views/feature/feature.js
@@ -1,10 +1,24 @@
 function yieldFeature() {
-  const outerBox = $("<div></div>");
+  const outerBox = $("<div style='min-width=500px; min-heigth=500px;'></div>");
   const draggingBox = $("<div></div>");
+  outerBox.css(outerBoxConfig.styles);
+  draggingBox.css(draggingBoxConfig.styles);
+
+  draggingBox.on("mousedown", draggingBoxConfig.dragCallback);
+
   outerBox.append(draggingBox);
   return outerBox;
 }
 
-function _setOuterBox() {}
+const outerBoxConfig = {
+  styles: {
+    border: "solid blue 2px",
+  },
+};
 
-function _setInnerBox() {}
+const draggingBoxConfig = {
+  styles: {},
+  dragCallback: function (e) {
+    console.log("hello");
+  },
+};
